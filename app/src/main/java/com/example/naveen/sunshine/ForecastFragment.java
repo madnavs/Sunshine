38ed0e2edcada73aba5df7f01e0d65218d283d89
Naveen <naveensai.madiraju@gmail.com>
2016-04-18 09:37:57 +0530

    Initial

---
+++ app/src/main/java/com/example/naveen/sunshine/ForecastFragment.java
@@ -0,0 +1,136 @@
+package com.example.naveen.sunshine;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ForecastFragment extends Fragment {
+
+    public ForecastFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+
+
+        String[] forecastArray = {
+                "Today-Sunny-44/3bv",
+                "Today-Sunny-44/3c",
+                "Today-Sunny-44/3x",
+                "Today-Sunny-44/3x",
+                "Today-Sunny-44/31",
+                "Today-Sunny-44/32",
+                "Today-Sunny-44/3c",
+                "Today-Sunny-44/3x",
+                "Today-Sunny-44/3x",
+                "Today-Sunny-44/31",
+                "Today-Sunny-44/32"
+        };
+        ArrayList<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+                                                                            R.layout.list_item_forecast,
+                                                                            R.id.list_item_forecast_textview,
+                                                                            weekForecast);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
+
+        listView.setAdapter(adapter)   ;
+
+
+
+
+        return rootView;
+    }
+
+    public class FetchWeatherTask extends AsyncTask{
+        @Override
+        protected Object doInBackground(Object[] objects) {
+
+
+            // These two need to be declared outside the try/catch
+            // so that they can be closed in the finally block.
+            HttpURLConnection urlConnection = null;
+            BufferedReader reader = null;
+
+            // Will contain the raw JSON response as a string.
+            String forecastJsonStr = null;
+
+            try {
+                // Construct the URL for the OpenWeatherMap query
+                // Possible parameters are avaiable at OWM's forecast API page, at
+                // http://openweathermap.org/API#forecast
+                String baseUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=19&lon=72&cnt=7&units=metric&mode=json";
+                String apiKey = "&APPID=" + BuildConfig.OPEN_WEATHER_MAP_API_KEY;
+                URL url = new URL(baseUrl.concat(apiKey));
+
+                // Create the request to OpenWeatherMap, and open the connection
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+
+                // Read the input stream into a String
+                InputStream inputStream = urlConnection.getInputStream();
+                StringBuffer buffer = new StringBuffer();
+                if (inputStream == null) {
+                    // Nothing to do.
+                    return null;
+                }
+                reader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                    // But it does make debugging a *lot* easier if you print out the completed
+                    // buffer for debugging.
+                    buffer.append(line + "\n");
+                }
+
+                if (buffer.length() == 0) {
+                    // Stream was empty.  No point in parsing.
+                    return null;
+                }
+                forecastJsonStr = buffer.toString();
+            } catch (IOException e) {
+                Log.e("PlaceholderFragment", "Error ", e);
+                // If the code didn't successfully get the weather data, there's no point in attemping
+                // to parse it.
+                return null;
+            } finally{
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+                if (reader != null) {
+                    try {
+                        reader.close();
+                    } catch (final IOException e) {
+                        Log.e("PlaceholderFragment", "Error closing stream", e);
+                    }
+                }
+            }
+            return null;
+        }
+
+
+    }
+    }
+
